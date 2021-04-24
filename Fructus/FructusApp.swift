ebd5befa81e87ace1c95e744a2434b18e2b5250e
Rajat Nagvenker <rajatnagvenker@gmail.com>
2021-04-24 19:23:26 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rajat Nagvenker on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
